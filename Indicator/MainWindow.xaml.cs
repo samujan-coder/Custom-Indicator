6f1f9dd98c5c2a4c02e3ca008be733c16ccc029c
Артём Самунджян <samujan@yandex.ru>
2022-01-21 13:47:14 +0300

    Исправление небольших ошибок

--- Indicator/MainWindow.xaml.cs
+++ Indicator/MainWindow.xaml.cs
@@ -3,22 +3,9 @@ using Indicator.Resource;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TradersToolbox.Core;
 using TradersToolbox.Core.Serializable;
-using System.Linq;
 using DevExpress.Mvvm.Native;
-using System.Collections.ObjectModel;
 using Indicator.Data;
 
 namespace Indicator
